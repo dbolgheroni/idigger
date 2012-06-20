2416e76cb5adde01022baab9db12c01a5d75d192
Daniel Bolgheroni <dbolgheroni@gmail.com>
2012-06-20 20:10:55 -0300

    First working revision.
    Very, very beta yet, has a TODO list in main file itself.

---
+++ src/idigger.pl
@@ -0,0 +1,140 @@
+#!/usr/bin/perl -w
+#
+# Copyright (c) 2012, Daniel Bolgheroni. All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+# 
+#   1. Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in
+#      the documentation and/or other materials provided with the
+#      distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY DANIEL BOLGHERONI ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DANIEL BOLGHERONI OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# TODO
+# x interface to functions
+# x functions prototypes
+# x module/package (?)
+# x log file
+# x parameter to only download stock info if asked to
+
+use strict;
+#use WWW::Curl::Easy;
+use Getopt::Std;
+use CGI qw/:standard/;
+
+#my $curl = new WWW::Curl::Easy;
+#$curl->setopt(CURLOPT_URL, 'http://www.guiainvest.com.br/raiox/abcb4.aspx');
+#open (my $FILE, ">", "curl_output.txt");
+#$curl->setopt(CURLOPT_WRITEDATA, $FILE);
+#$curl->perform;
+
+my (@stock);
+
+# returns the stock list stored in config file
+# TODO: prototype to put constraints on the parameter list
+sub init_stock_conf {
+    my $stock;
+    
+    open (STOCKCONF, "<", "$ENV{HOME}/.idigger/stock.conf") ||
+        die "can't open .stock.conf\n";
+
+    while (<STOCKCONF>) {
+        $stock = lc; # lowercase
+        push @_, $stock;
+    }
+    chomp @_;
+
+    close (STOCKCONF);
+    return @_;
+}
+
+# downloads the stock info of the stocks passed as a parameter and
+# can be used with the LIST returned by init_stock_conf
+# TODO: prototype to put constraints on the parameter list
+sub _gi_dl_stock_info {
+    my ($ret, $stock);
+
+    foreach (@_) {
+        $ret = system ("curl -s \'http://www.guiainvest.com.br/raiox/$_.aspx\' > $ENV{HOME}/.idigger/rawdata/$_.aspx");
+
+        $stock = lc $_;
+        if ($ret == 0) {
+            print "$stock info downloaded OK\n";
+        } else {
+            print "$stock info download FAILED\n";
+        }
+    }
+}
+
+# P/E ratio
+# returns a hash of P/E ordered from lower to higher
+# note: lower is better 
+# TODO: prototype to put constraints on the parameter list
+sub _gi_get_current_pe {
+    my (%pe, $stock, $key);
+
+    foreach $stock (@_) {
+        open (STOCK, "<", "$ENV{HOME}/.idigger/rawdata/$stock.aspx") ||
+            die "can't open $stock.aspx\n";
+
+        while (<STOCK>) {
+            if (/lbPrecoLucroAtual/) {
+                s/.*\">//;
+                s/<.*//;
+                s/,/./;
+                chomp;
+                $pe{$stock} = $_;
+            }
+        }
+    }
+    
+    return %pe;
+}
+
+# check for command line options
+our ($opt_h, $opt_g);
+getopts('h:g');
+
+# initializes some data structures
+@stock = init_stock_conf;
+
+# real processing starts here
+_gi_dl_stock_info (@stock);
+my %pe = _gi_get_current_pe (@stock);
+
+print start_html('invest system');
+print h1('Rela&ccedil;&atilde;o P/L');
+
+print "<table border=1>\n";
+print "<tr bgcolor=#c0c0c0><th>A&ccedil;&atilde;o</th><th>P/L</th><tr>\n";
+
+foreach my $key (sort keys %pe) {
+    print "<tr><td>$key</td>";
+
+    if (($pe{$key} < 6.0) && ($pe{$key} >= 0.0)) {
+        print "<td style=\"background-color:lightgreen\">",
+              uc ($pe{$key}), "</td></tr>\n";
+    } else {
+        print "<td>", uc ($pe{$key}), "</td></tr>\n";
+    }
+}
+
+print "</table>\n";
+
+print end_html;
