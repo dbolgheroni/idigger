0d64aa764c18c555e32e4fb3c1570795b32aea88
Daniel Bolgheroni <dbolgheroni@gmail.com>
2012-10-17 16:14:54 -0300

    It makes no sense to have a class in this module.

--- src/gi.py
+++ src/gi.py
@@ -5,72 +5,67 @@ import os
 import re
 import urllib.request
 
-class GuiaInvest:
-    relat = os.path.join(os.environ['HOME'], ".idigger", "rawdata")
+relat = os.path.join(os.environ['HOME'], ".idigger", "rawdata")
 
-    # class methods
-    @classmethod
-    def fetch(cls, c):
-        """fetch raw data to extract info needed by other classes"""
+# class methods
+def fetch(c):
+    """fetch raw data to extract info needed by other classes"""
 
-        print("downloading", c.rjust(6), end=" stock info... ")
+    print("downloading", c.rjust(6), end=" stock info... ")
 
-        # define path for local file
-        absol = os.path.join(cls.relat, c.lower() + ".aspx")
+    # define path for local file
+    absol = os.path.join(relat, c.lower() + ".aspx")
 
-        # define URL
-        baseurl = "http://guiainvest.com.br/raiox/"
-        url = baseurl + c.lower() + '.aspx' 
+    # define URL
+    baseurl = "http://guiainvest.com.br/raiox/"
+    url = baseurl + c.lower() + '.aspx' 
 
-        # download URL
-        try:
-            iurl = urllib.request.urlopen(url)
-        except URLError:
-            print(end="FAILED")
-        else:
-            print(end="OK")
-        
-        # write file
-        try:
-            ourl = open(absol, "w")
-        except IOError:
-            print(" (couldn't write to local file)")
-        else:
-            print()
+    # download URL
+    try:
+        iurl = urllib.request.urlopen(url)
+    except URLError:
+        print(end="FAILED")
+    else:
+        print(end="OK")
 
-        stock = iurl.read().decode("iso-8859-1")
-        ourl.write(stock)
+    # write file
+    try:
+        ourl = open(absol, "w")
+    except IOError:
+        print(" (couldn't write to local file)")
+    else:
+        print()
 
-        # close descriptors
-        iurl.close()
-        ourl.close()
+    stock = iurl.read().decode("iso-8859-1")
+    ourl.write(stock)
 
-    @classmethod
-    def extract_pe(cls, stock):
-        value = cls.__extract_id(stock,
-                'lbPrecoLucroAtual')
-        return value
+    # close descriptors
+    iurl.close()
+    ourl.close()
 
-    @classmethod
-    def extract_roe(cls, stock):
-        value = cls.__extract_id(stock,
-                'lbRentabilidadePatrimonioLiquido3')
-        return value
+def extract_pe(stock):
+    value = __extract_id(stock,
+            'lbPrecoLucroAtual')
+    return value
 
-    @classmethod
-    def __extract_id(cls, stock, v):
-        value = -999
+def extract_roe(stock):
+    value = __extract_id(stock,
+            'lbRentabilidadePatrimonioLiquido3')
+    return value
+
+def __extract_id(stock, v):
+    value = -999
+
+    path = os.path.join(relat, stock + ".aspx")
+    f = open(path, encoding="iso-8859-1")
+    s = f.read()
     
-        path = os.path.join(cls.relat, stock + ".aspx")
-        f = open(path, encoding="iso-8859-1")
-        s = f.read()
-        
-        regex = v + r'">(?P<value>(-)?(\d)+,(\d)+)'
-        pattern = re.compile(regex)
-        match = pattern.search(s)
-        if match:
-            r = match.group('value')
-            value = float(re.sub(r',', r'.', r))
-    
-        f.close()
-        return value
+    regex = v + r'">(?P<value>(-)?(\d)+,(\d)+)'
+    pattern = re.compile(regex)
+    match = pattern.search(s)
+    if match:
+        r = match.group('value')
+        value = float(re.sub(r',', r'.', r))
+
+    f.close()
+    return value
