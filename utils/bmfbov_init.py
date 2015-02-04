f2aa07f288d0b1d76f0c2bbd9614c258298c9b5c
Daniel Bolgheroni <dbolgheroni@gmail.com>
2015-02-04 10:57:59 -0200

    Add script to initialize stocks.
    The index used now is IBrA and this is reflected in the url used in the
    script.

---
+++ utils/bmfbov_init.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#
+# IMPORTANT: For some reason, sometimes this script needs to run more
+# than one time, since the url often fails to respond at the first try.
+
+import sys
+import urllib.request
+from html.parser import HTMLParser
+
+class StockParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.inTag = None;
+
+    def handle_starttag(self, tag, attrs):
+        for name, value in attrs:
+            if 'lblCodigo' in value:
+                self.inTag = True;
+
+    def handle_data(self, data):
+        if self.inTag:
+            stocks.append(data)
+
+    def handle_endtag(self, tag):
+        self.inTag = False
+
+# urlopen() returns a byte object, since it can't automatically
+# determine the encoding it receives from the server. So, we use
+# decode(), since it's specified in the content.
+try:
+    html = urllib.request.urlopen("http://www.bmfbovespa.com.br/indices/ResumoCarteiraTeorica.aspx?Indice=IBrA&idioma=pt-br").read().decode('utf-8')
+except ConnectionResetError:
+    print("can't connect to BMF&Bovespa, try running it again")
+    exit(1)
+
+stocks = []
+parser = StockParser()
+parser.feed(html)
+
+for s in stocks:
+    print(s)
+# use a file as output
+#outfile = "out.conf"
+#with open(outfile, "wt") as f:
+#    for s in stocks:
+#        print(s, file=f)
